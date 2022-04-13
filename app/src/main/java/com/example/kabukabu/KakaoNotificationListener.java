a1d70e0b415dc33c62367f06b4736e779d4e7623
sioux98 <dlshgus789@gmail.com>
2022-04-13 20:55:18 +0900

    test

--- app/src/main/java/com/example/kabukabu/KakaoNotificationListener.java
+++ app/src/main/java/com/example/kabukabu/KakaoNotificationListener.java
@@ -1,27 +1,37 @@
 package com.example.kabukabu;
 
+
+import android.app.Notification;
+import android.content.Intent;
+import android.os.Build;
 import android.app.Notification;
 import android.app.Service;
 import android.os.Bundle;
 import android.service.notification.NotificationListenerService;
 import android.service.notification.StatusBarNotification;
 import android.text.TextUtils;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class KakaoNotificationListener extends NotificationListenerService {
-    @Override
-    public void onNotificationPosted(StatusBarNotification sbn){
-        final String packageName = sbn.getPackageName();
+import androidx.annotation.RequiresApi;
 
-        if(!TextUtils.isEmpty(packageName) && packageName.equals("com.kaako.talk")){
+@RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR2)
+public class KakaoNotificationListener extends NotificationListenerService {
+
+    @Override
+    public void onNotificationPosted(StatusBarNotification sbn) {
+
+        final String packageName = sbn.getPackageName();
+        if (!TextUtils.isEmpty(packageName) && packageName.equals("com.kakao.talk")) {
             //알람 확인
-                        Notification notification = sbn.getNotification();
+            Notification notification = sbn.getNotification();
+
             Bundle extras = notification.extras;
             String title = extras.getString(Notification.EXTRA_TITLE);
             String text = extras.getString(Notification.EXTRA_TEXT);
-            String subtext = extras.getString(Notification.EXTRA_SUB_TEXT);
-            Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
+
+            Toast.makeText(this, "왜 안돼 ㅡㅡ", Toast.LENGTH_SHORT).show();
+
         }
     }
-
 }

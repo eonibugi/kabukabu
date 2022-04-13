9eda8921366822136f3e8f11720b9b9f4f049791
sioux98 <dlshgus789@gmail.com>
2022-04-13 20:40:57 +0900

    test

--- app/src/main/java/com/example/kabukabu/KakaoNotificationListener.java
+++ app/src/main/java/com/example/kabukabu/KakaoNotificationListener.java
@@ -12,14 +12,15 @@ public class KakaoNotificationListener extends NotificationListenerService {
     @Override
     public void onNotificationPosted(StatusBarNotification sbn){
         final String packageName = sbn.getPackageName();
+
         if(!TextUtils.isEmpty(packageName) && packageName.equals("com.kaako.talk")){
             //알람 확인
-            Toast.makeText(this, "메시지 발생", Toast.LENGTH_SHORT).show();
-            Notification notificatin = sbn.getNotification();
-            Bundle extras = notificatin.extras;
+                        Notification notification = sbn.getNotification();
+            Bundle extras = notification.extras;
             String title = extras.getString(Notification.EXTRA_TITLE);
             String text = extras.getString(Notification.EXTRA_TEXT);
             String subtext = extras.getString(Notification.EXTRA_SUB_TEXT);
+            Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
         }
     }
 
--- app/src/main/java/com/example/kabukabu/MainActivity.java
+++ app/src/main/java/com/example/kabukabu/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.kabukabu;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationManagerCompat;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.speech.tts.TextToSpeech;
 
 import java.util.Locale;
+import java.util.Set;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -16,6 +19,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        boolean isPermissionAllowed = isNotiPermissionAllowed();
+        if(!isPermissionAllowed) {
+            Intent intent = new Intent("android.settings.ACTION_NOTIFICATION_LISTENER_SETTINGS");
+            startActivity(intent);
+        }
+        startActivity(new Intent("android.settings.ACTION_NOTIFICATION_LISTENER_SETTINGS"));
         tts = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
             @Override
             public void onInit(int status) {
@@ -24,6 +33,22 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+
+    }
+    private boolean isNotiPermissionAllowed() {
+        Set<String> notiListenerSet = NotificationManagerCompat.getEnabledListenerPackages(this);
+        String myPackageName = getPackageName();
+
+        for(String packageName : notiListenerSet) {
+            if(packageName == null) {
+                continue;
+            }
+            if(packageName.equals(myPackageName)) {
+                return true;
+            }
+        }
+
+        return false;
     }
 
 }

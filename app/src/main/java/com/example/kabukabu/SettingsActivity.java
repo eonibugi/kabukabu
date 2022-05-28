fa3a3a0636a7571e814237c593b80560fb1b7cf8
hjHwang12 <hyejeong1209@kyonggi.ac.kr>
2022-05-29 02:24:20 +0900

    (seekbar+폰 버튼)볼륨 조절 기능

--- app/src/main/java/com/example/kabukabu/SettingsActivity.java
+++ app/src/main/java/com/example/kabukabu/SettingsActivity.java
@@ -4,23 +4,29 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
+import android.media.AudioManager;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.SeekBar;
 import android.widget.Switch;
 import android.widget.Toast;
 
 public class SettingsActivity extends AppCompatActivity {
 
     Switch swichView;
+    SeekBar volumeBar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        AudioManager audioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
 
         ImageButton Timeline_btn = (ImageButton)findViewById(R.id.timeline_button);
         Timeline_btn.setOnClickListener(new View.OnClickListener() {
@@ -31,6 +37,57 @@ public class SettingsActivity extends AppCompatActivity {
             }
         });
 
+        volumeBar = findViewById(R.id.volume);
+
+
+        int nMax = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+        int nCurrentVolumn = audioManager
+                .getStreamVolume(AudioManager.STREAM_MUSIC);  // 현재 볼륨 가져오기
+
+        volumeBar.setMax(nMax);
+        volumeBar.setProgress(nCurrentVolumn);
+
+        volumeBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC,
+                        progress, 0);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
     }
 
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) { //스마트폰 옆에 버튼으로 볼륨조절하는 것
+        AudioManager audioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
+        switch (keyCode) {
+
+            case KeyEvent.KEYCODE_VOLUME_UP:
+
+                audioManager.adjustStreamVolume(AudioManager.STREAM_MUSIC, AudioManager.ADJUST_RAISE, AudioManager.FLAG_SHOW_UI);
+
+                return true;
+
+            case KeyEvent.KEYCODE_VOLUME_DOWN:
+
+                audioManager.adjustStreamVolume(AudioManager.STREAM_MUSIC, AudioManager.ADJUST_LOWER, AudioManager.FLAG_SHOW_UI);
+
+                return true;
+
+            default:
+                return false;
+
+        }
+
+    }
 }
